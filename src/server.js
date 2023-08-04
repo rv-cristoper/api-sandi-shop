0ed166af9cd2324caa846500ef315e39787677c7
cristoper <cristoper.runco@banexcoin.com>
2023-08-04 09:57:12 -0500

    .

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-import open from "open"
 import app from "./config/app.js";
 import { initSocket } from "./config/socket.js";
 import config from './config/index.js'
@@ -8,7 +7,6 @@ const logger = getLogger();
 const server = app.listen(config.port, () => {
     // console.log('Servidor ejecutandose en el puerto: ', config.port)
     logger.debug(`Servidor ejecutandose en http://localhost:${config.port} en ${config.nodeEnv}.`)
-    open(`http://localhost:${config.port}/login`)
 })
 server.on('error', error => console.log('Error en el servidor', error))
 
